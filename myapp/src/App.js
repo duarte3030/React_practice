933623f2d1b7750bd71bd53817688e823c0394e5
Guilherme <gduarte3030@gmail.com>
2023-07-30 20:24:54 -0300

    updated

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,30 +1,62 @@
-import './App.css';
+import { useEffect } from "react";
+import './App.css'
+import SearchIcon from './search.svg'
 
 
-const Person = () => {
-  return (
-    <>
-      <h2>Name: John</h2>
-      <h3>Last Name: Doe</h3>
-      <h3>Age: 30</h3>
-    </>
-  )
+const API_URL = 'http://www.omdbapi.com?apikey=d92ecaff'
+
+const movie1 = {
+    "Title": "Spiderman",
+    "Year": "1990",
+    "imdbID": "tt0100669",
+    "Type": "movie",
+    "Poster": "N/A"
 }
 
-
 const App = () => {
- 
-  return (
-    <div className="App">
-      <h1>
-        <Person/>
-        <Person/>
-        <Person/>
-        <Person/>
-        <Person/>
-      </h1>
-    </div>
-  );
+    
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('Spiderman');
+    }, [])
+
+    return (
+        <div className='app'>
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input
+                placeholder="Search for movies"
+                value="Superman"
+                onChange={() => {}}
+                />
+                <img
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => {}}
+                />
+            </div>
+
+            <div className="container">
+                <div className="movie">
+                    <div>
+                        <p>{movie1.Year}</p> 
+                    </div>
+
+                    <div>
+                        <img src={movie1.Poster} alt={movie1.title}/>
+                    </div>
+                </div> 
+            </div>
+        </div>
+
+    );
 }
 
 export default App;
